713bd3da2872f6e7cc0c779fc161c51878d2926a
bagasadit123 <bagasadit430@gmail.com>
2026-03-31 10:30:42 +0700

    deklarasi variabel global

--- megersort.cpp
+++ megersort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int arr [20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <=20)
+        {
+            break;
+        }
+    }
+}
